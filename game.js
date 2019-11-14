227f49f0c39c86c259ce41d41ac8d0789060d77b
Shree-hari <harshmacc@gmail.com>
2019-11-14 18:58:18 +0530

    animation and sound added

--- game.js
+++ game.js
@@ -4,5 +4,7 @@ function nextSequence() {
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
-
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+  audio.play();
 }
